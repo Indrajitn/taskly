d79dc475e27d479fc4f28c63e96d4e8be1967cd3
Indrajitn <indrajitmaji.20.cs@iite.indusuni.ac.in>
2025-09-16 15:24:43 +0530

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,46 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Taskly",
+  description: "Project Management App",
+};
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme:shadesOfPurple,
+        variables: {
+          colorPrimary: "#3b82f6",
+          colorBackground: "#1a202c",
+          colorInputBackground: "#2D3748",
+          colorInputText: "#F3F4F6",
+        },
+        elements: {
+          formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
+          card: "bg-gray-800",
+          headerTitle: "text-blue-400",
+          headerSubtitle: "text-gray-400",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} animated-dotted-background`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">{children}</main>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
